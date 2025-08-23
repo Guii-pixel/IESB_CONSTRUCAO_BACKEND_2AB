caabdfe8a789df62a8f61716cb048e52566c869f
Guii-pixel <guilherme07play@gmai.com>
2025-08-22 21:40:22 -0300

    Adicionando arquivos e arquivo de calculadora de nota do IESB

---
+++ PRATICAS/01-primeiro-projeto/index.js
@@ -0,0 +1,18 @@
+console.log("Meu primeiro projeto!!")
+
+console.log("Hello!")
+
+let nome = 'Guilherme'
+
+let sobreNome = 'ALMEIDA'
+
+let idade = 21
+
+console.log("Olá " + nome + " " + sobreNome)
+console.log("Sua idade é " + idade)
+
+if(idade => 18){
+    console.log("Voce é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
